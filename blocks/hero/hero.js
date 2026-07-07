494ac755df2075137c4e78d9215f08c2ea5eff83
anthonycg <anthonygibson24@gmail.com>
2026-07-07 11:06:01 -0500

    hero

---
+++ blocks/hero/hero.js
@@ -0,0 +1,18 @@
+export default function decorate(block) {
+  const rows = [...block.children];
+
+  block.classList.add("hero-wrapper");
+
+  rows.forEach((row) => {
+    const cells = [...row.children];
+
+    cells.forEach((cell) => {
+      const picture = cell.querySelector("picture");
+      if (picture) {
+        cell.classList.add("hero-image");
+      } else {
+        cell.classList.add("hero-content");
+      }
+    });
+  });
+}
