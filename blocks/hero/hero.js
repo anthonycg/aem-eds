d9a445b0751c8535196bfcba0b895401a7089741
anthonycg <anthonygibson24@gmail.com>
2026-07-08 10:04:35 -0500

    rm wrapper

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,13 +1,12 @@
 export default function decorate(block) {
   const rows = [...block.children];
 
-  block.classList.add("hero-wrapper");
-
   rows.forEach((row) => {
     const cells = [...row.children];
 
-    cells.forEach((cell, index) => {
-      if (index === 0) {
+    cells.forEach((cell) => {
+      const picture = cell.querySelector("picture");
+      if (picture) {
         cell.classList.add("hero-image");
       } else {
         cell.classList.add("hero-content");
